b4d44f5679e53cb0793440ff760857fb02b8798e
alexjamesmx <sg.alex32@gmail.com>
2024-09-14 23:01:07 -0600

    create user

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,29 +1,39 @@
 // src/pages/CardPage.tsx
 // src/pages/CardPage.tsx
-import React from 'react';
-import Card from './components/card'; 
-import Navbar from './components/navbar';
-import Footer from './components/footer';
+import React, { useEffect, useState } from "react";
+import Card from "./components/card";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import { useAuth } from "@clerk/clerk-react";
+import { existsUser } from "./api/clerk.js";
 
 const Dashboard: React.FC = () => {
+  const { userId } = useAuth(); // Fetch the current session (token) from Clerk
+
+  useEffect(() => {
+    if (userId) {
+      console.log("Fetching user data for user ID:", userId);
+      const fetchData = async () => {
+        try {
+          const data = await existsUser(userId);
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [userId]);
   return (
     <div className="flex flex-col min-h-screen font-serif">
       <Navbar />
-      <div className='pt-8'>
+      <div className="pt-8">
         <h1 className="text-4xl font-bold text-center pt-9">Your Events</h1>
       </div>
       <main className="flex-grow pt-12 pb-8 px-4">
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <Card 
-            title="Marias Wedding" 
-            author="Celeste Mills" 
-            isPaid={true}
-          />
-          <Card 
-            title="Johns Graduation" 
-            author="John Doe" 
-            isPaid={false}
-          />
+          <Card title="Marias Wedding" author="Celeste Mills" isPaid={true} />
+          <Card title="Johns Graduation" author="John Doe" isPaid={false} />
           {/* Add more Card components as needed */}
         </section>
       </main>
